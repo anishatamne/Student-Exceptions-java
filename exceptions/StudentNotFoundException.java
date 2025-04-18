713aac3878d96a9214cad8e8231edca61d5423db
Anisha Tamne <anishatamne@gmail.com>
2025-04-18 22:03:34 +0530

    defining student not found exception

---
+++ exceptions/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package exceptions;
+//StudentNotFoundException
+public class StudentNotFoundException extends RuntimeException {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
+
