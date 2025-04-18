9a49a344bec5f74ea6adca91f1c5709249e530d9
Anisha Tamne <anishatamne@gmail.com>
2025-04-18 21:48:00 +0530

    function to add a student

---
+++ StudentManager.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import exceptions.*;
+
+public class StudentManager {
+    private final ArrayList<Student> students = new ArrayList<>();
+    private final Scanner sc = new Scanner(System.in);
+
+    public void addStudent() {
+        System.out.println("Enter PRN:");
+        String prn = sc.next();
+        for (Student s : students) {
+            if (s.prn.equals(prn)) {
+                throw new DuplicateStudentException("Student with PRN already exists!");
+            }
+        }
+
+        System.out.println("Enter Name:");
+        String name = sc.next();
+        System.out.println("Enter DOB:");
+        String dob = sc.next();
+        System.out.println("Enter Marks:");
+        double marks = sc.nextDouble();
+
+        students.add(new Student(prn, name, dob, marks));
+        System.out.println("Student added successfully.");
+    }
